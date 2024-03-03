3b0b7b2d20695aa667c0e84f5f87cf25c6cefe54
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-03 21:20:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,30 @@
 #include<iostream>
 
-// 선언(declaration)
-extern int x; // 객체 선언
-std::size_t numDigits(int num); // 함수 선언
-class Widget; // 클래스 선언
-template<typename T> // 템플릿 선언
-class GrapNode; 
-
-// 정의(definition) 
-int x; // 객체 정의
-std::size_t numDigits(int num) { // 함수 정의
-	std::size_t digitsSoFar = 1;
-
-	while ((num /= 10) != 0) ++digitsSoFar;
-	return digitsSoFar;
-}
-class Widget { // 클래스 정의
+// 초기화(initialization)
+class A {
 public:
-	Widget();
-	~Widget();
+	A() {} // 기본 생성자(default constructor)
 };
-template<typename T> // 템플릿 정의
-class GrapNode {
+
+class B {
 public:
-	GrapNode();
-	~GrapNode();
+	explicit B(int n = 0, bool b = true) {} // 기본 생성자(default constructor)
 };
+
+class C {
+public:
+	explicit C(int x) {} // 기본 생성자 아님
+};
+
+void dosomething(B bObject) {}
 
 int main() {
+	B bObj1;
+	dosomething(bObj1); // 문제 없는 코드
 	
+	B bobj2(28);	// 괜찮은 코드, x인자가 0대신 28이 들어감 (bool은 그대로 true가 들어감)
+	dosomething(28);	//컴파일 에러! -> int에서 B로 전환활 생성자가 없음.
+				// >>> 다른 자료형으로의 암시적인 변환을 막는 키워드 - explicit
+
+	dosomething(B(28)); // 괜찮은 코드 int -> B로 명시적인 변환을 함!
 }
