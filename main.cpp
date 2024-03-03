859a5cdbebf9ce45c46b3407b9f19ed0e1bb0dc2
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-03 21:23:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,29 @@
-#include<iostream>
+#include <iostream>
 
-class Widget {
+// C++ STL(Standard Template Library)
+// 구성요소
+// - 컨테이너(container)
+// - 반복자(iterator)
+// - 알고리즘(algorithm)
+// + 함수 객체(function objection)
+
+class A { // 함수 객체의 기본 구성
 public:
-	Widget();	// 기본 생성자(default ctor)
-	Widget(const Widget& rhs); // 복사 생성자
-	Widget& operator=(const Widget& rhs); // 복사 할당(대입) 연산자(copy assignment operator)
+	void operator()(int n)
+	{ 
+		std::cout << "데이터 - " << n << "을 처리함" << '\n';
+	}
 };
 
-Widget::Widget() 
-{ 
-	std::cout << "default ctor" << '\n'; 
-}
-
-Widget::Widget(const Widget& rhs) 
-{ 
-	std::cout << "copy ctor" << '\n'; 
-}
-
-Widget& Widget::operator=(const Widget& rhs) 
-{ 
-	std::cout << "copy assignment operator" << '\n'; return *this; 
-}
-
-
-void dosomething(Widget w) 
-{ 
-	std::cout << "doing somthing with Widget Object" << '\n'; 
-}
-
 int main() {
-	Widget w1;
+	std::vector<int> vec{1,4,3,2,5}; // container
+	
+	for (std::vector<int>::iterator itr = vec.begin(); itr != vec.end(); ++itr) // iterator
+		std::cout << *itr << ' ';
+	std::cout << '\n';
 
-	dosomething(w1); // 값의 의한 전달(pass-by-value) == 복사 생성자 호출(copy ctor calls)
+	std::sort(vec.begin(), vec.end()); // algorithm
+
+	A a;
+	a(3); // function object
 }
-
