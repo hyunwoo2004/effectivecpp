4fd6dae6538f31e8f2e83151e4b79d2634a75f4d
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-05 22:00:32 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 
-// 미정의 동작(undefined behavior) <대표적으로>
-// 1. null pointer를 역참조(*)할 때
-// 2. 유효하지 않은 배열의 원소지정 번호를 참조하려 할 때
-//	-> 미정의 동작은 매우 위험하므로 하지 않도록 주의하자!
+// C++ interface
+// 언어의 차원에서 주어져 있지만, C++에는 없음
+// 그러나 C++로도 인터페이스와 흡사하게 할 수 있음
+// 보통 함수의 시그니처, 어떤 클래스의 접근가능 요소
+// + 템플릿의 타입 매개변수로서 유효해야 하는 표현식 등
+
+//함수의 시그니처
+// 1. 함수의 이름 f
+// 2. 매개변수의 개수 2
+// 3. 매개변수의 자료형 (int, double)
+void f(int n, double d) {}
+
+class A {
+public: //----]
+protected://--|--> // 접근 가능 요소
+private://----]
+};
 
 int main() {
-	int* p = nullptr; // p는 nullptr이다.
-	std::cout << *p; // 경우 1
-	
-	char name[] = "Darla"; // 배열의 범위: 0 ~ 5
 
-	char c = name[10]; // 경우 2
 }
