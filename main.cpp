ada9375cb74c93bb720ec87e0fa8ce4523d09223
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-05 22:15:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 // 항목 1: C++ 언어를 연합체로 바라보는 안목은 필수
 
 // C++ 구성
-// 객체 지향 개념의 C++
-// -> '클래스를 쓰는 C'에 관한 것이 모두 해당됨
-// -> 클래스, 캡슐화, 상속, 다형성, 가상 함수 등
+// 템플릿 C++
+// C++의 일반화 프로그래밍(generic programming)의 부분, 많은 프로그래머들이 경험해보지 않은 영역 중 하나
+// + 템플릿 메타 프로그래밍(TMP, Template Meta Programming): 일반 C++ 프로그래밍과는 맞닿아 돌아가지 않는 다른 방법의 프로그래밍
 
-class A { // 클래스
-  int n; // member variable
+class A { 
+  int n;
 public:
-  void f() { //member function
+  void f() { 
   }
 }
 
