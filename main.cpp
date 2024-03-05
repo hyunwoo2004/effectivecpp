926ae4bf75a7f1eec704e5f056b78341468142f2
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-05 22:22:35 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,11 @@
 // 항목 1: C++ 언어를 연합체로 바라보는 안목은 필수
 
 // C++ 구성
-// 템플릿 C++
-// C++의 일반화 프로그래밍(generic programming)의 부분, 많은 프로그래머들이 경험해보지 않은 영역 중 하나
-// + 템플릿 메타 프로그래밍(TMP, Template Meta Programming): 일반 C++ 프로그래밍과는 맞닿아 돌아가지 않는 다른 방법의 프로그래밍
-
-class A { 
-  int n;
-public:
-  void f() { 
-  }
-}
+// STL
+// Standard Template Language의 약자로, 말 그대로 템플릿 라이브러리이다.
+// 컨테이너(container), 반복자(iterator), 알고리즘(algorithm), 함수 객체(function object)들이 서로 엄청 얽혀있도록
+// 돌아가는 것이 규약, 그러나 템플릿과 라이브러리는 얼마든지 다른 아이디어를 중심으로 만들어질 수 있음
+// 또한 STL만의 사용 규칙이 있으니 사용할 때 규칙만 잘 지키자!
 
 int main() {
   
