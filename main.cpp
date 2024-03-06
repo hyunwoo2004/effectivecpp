fac7f56af5da1779a472fbb126562547b85f9dbb
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-06 21:46:30 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,28 +5,15 @@
 // 항목 2: #define을 쓰려거든 const, enum, inline을 떠올리자
 // -> 다른 말로, '가급적 선행 처리자보다 컴파일러를 더 가까이 하자'이다.
 
-// #define 에서 상수로 교체할 때 주의할 점
-// 2. 클래스 멤버로 상수를 정의하는 경우 (클래스 상수를 정의하는 경우) --> 그렇다고 클래스 상수를 #define을 만든다는 생각은 버리
-// 어떤 상수의 유효 범위를 클래스로 한정하고자 할 때는 그 상수를 멤버로 만들어야 하는데, 
-// 그 상수의 사본 개수가 한 개를 넘지 못하게 하고 싶다면 정적(static) 멤버로 만들어야 함
-// 예시
+// 한가지 예외: 해당 클래스를 컴파일 하는 도중에 클래스 상수의 값이 필요할 때
+// -> 예시: 배열 멤버를 선언할 때가 대표적임
+// 이 때는 "나열자 둔갑술(enum hack)"이라는 통칭으로 멋스럽게 알려진 기법이 있음
+// 이 기법의 원리: 나열자(enumerator)타입의 값은 int가 놓일 곳에도 쓸 수 있다는 C++의 진실을 적극 활용하는 것
 class GamePlayer {
 private:
-  static const int NumTurns = 5;   // 상수 선언
-  int scores[NumTurns];            // 상수를 사용하는 부분
-};
-const int GamePlayer::NumTurns; // 상수 정의
+  enum {NumTurns = 5}; // "나열자 둔갑술": NumTurns를
+                       // 5에 대한 기호식 이름으로 만듦
 
-// 조금 오래된 컴파일러는 위의 문법이 받아들이지 않는 경우가 존재(선언된 시점에 값을 주는 것이 대개 맞지 않는다고 판단할 수 있음)
-// 이 때는 이렇게 해야 함 (특히 정수류가 아닌 자료형의 경우도 포함)
-class GamePlayer {
-private:
-  static const double HP;   // 상수 선언
- // ...
+  int scores[NumTurns];
+  //...
 };
-const double
-  GamePlayer::HP = 5.0; // 상수 정의
-
-int main() {
-  
-}
