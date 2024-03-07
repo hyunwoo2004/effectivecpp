f319e382115a0c3f104bb8986483e3bb68639c9f
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-07 22:31:43 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,8 @@
 // 1. C++에 왔으면 C++ 법을 따릅시다.
 // 항목 3: 낌새만 보이면 const를 들이대 보자!
 
-// 프로그랭에서 상수 객체가 생기는 경우
-// 1. 상수 객체에 대한 포인터
-// 2. 상수 객체에 대한 참조자로 객체가 전달될 때
+// operator[]를 '오버로드(overload)'해서 각 버전마다 반환 타입을 다르게 가져갔기 때문에, TextBlock의 상수 객체와 비상수 객체의 쓰임새가 달라짐
 // 예시
-void print(const TextBlock& ctb)      // 이 함수에서 ctb는 상수 객체로 쓰임
-{
-  std::cout << ctb[0];                // TextBlock::operator[]의 상수
-                                      // 멤버를 호출
-}
 
 
 
@@ -28,10 +21,14 @@ private:
 };
 
 int main() {
-  TextBlock tb{"Hello"};        // TextBlock::operator[]의
-  std::cout << tb[0];           // 비상수 멤버를 호출함
+  TextBlock tb{"Hello"};    
+  const TextBlock ctb{"World"};
 
-  const TextBlock ctb{"World"}; // TextBlock::operator[]의
-  std::cout << ctb[0];          // 상수 멤버를 호출함
+  std::cout << tb[0];     // OK, 비상수 버전의 TextBlock 객체를 읽음
   
+  tb[0] = 'x';            // OK, 비상수 버전의 TextBlock 객체를 씀
+  
+  std::cout << ctb[0];    // OK, 상수 버전의 TextBlock 객체를 씀
+
+  ctb[0] = 'x';           // 컴파일 에러!, 상수 버전의 TextBlock 객체에 대해 쓰기는 안됨
 }
