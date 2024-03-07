01ae76eb878ab2cf6eddaa037a9024f2c8d2e9e1
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-07 22:26:22 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,18 @@
 // 1. C++에 왔으면 C++ 법을 따릅시다.
 // 항목 3: 낌새만 보이면 const를 들이대 보자!
 
-// const키워드가 있고 없고의 차이만 있는 멤버 함수들은 오버로딩이 가능함
+// 프로그랭에서 상수 객체가 생기는 경우
+// 1. 상수 객체에 대한 포인터
+// 2. 상수 객체에 대한 참조자로 객체가 전달될 때
 // 예시
+void print(const TextBlock& ctb)      // 이 함수에서 ctb는 상수 객체로 쓰임
+{
+  std::cout << ctb[0];                // TextBlock::operator[]의 상수
+                                      // 멤버를 호출
+}
+
+
+
 class TextBlock {
 public:
   //...
