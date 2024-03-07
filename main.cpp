8d86518d8f8beccfeb67fb4c62ba149fe50725d3
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-07 21:52:31 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,16 @@
 // 1. C++에 왔으면 C++ 법을 따릅시다.
 // 항목 3: 낌새만 보이면 const를 들이대 보자!
 
-// STL 반복자(iterator)는 포인터를 본뜬 것이기 때문에, 기본적인 동작 원리가 T* 포인터와 진짜 흡사함
-// 어떤 반복자를 const로 선언하는 일은 포인터를 상수로 선언하는 것과 같음(const ...iterator* == T* const)
-// 반복자는 자신이 가리키는 대상이 아닌 것을 가리키는 경우가 허용되지 않지만, 반복자가 가리키는 대상 자체는 변경이 가능함
-// 만약 변경이 불가능한 객체를 가리키는 반복자가 필요하다면 const_iterator를 쓰면 됨
+// 가장 강력한 const의 용도: 함수 선언에 사용하는 경우
+// 함수 선언문에 const는 함수 반환 값, 각각의 매개변수, 멤버 함수 앞에 붙을 수 있고, 함수 전체에 대해 const의 성질을 붙일 수 있다.
+// 1. 함수 반환 값
+// 함수 반환 값을 상수롤 정해 주면, 안전성이나 효율을 포기하지 않고도 사용자측의 에러 돌발 상황을 줄여주는 효과를 꽤 자주 볼 수 있게 됨.
+// 예시: 유리수 클래스
+class Rational {/...};
+const Rational operator* (const Rational& lhs, const Rational& rhs);
+// *연산자의 반환 값이 상수 객체인 이유: 다음과 같은 이상한 실수를 없애는 데 도움
+Rational a, b, c;
 
-int main() {
-  std::vector<int> vec;
-  const std::vector<int>::iterator iter = vec.begin();
-                                          // iter는 T* const처럼 동작
-  *iter = 10;                             // OK, iter가 가리키는 대상을 변경
-  ++iter;                                 // 에러! iter는 상수입니다
+(a * b) = c; // a*b의 결과에 대고 =를 호출하면 안됨!
 
-  std::vector<int>::const_iterator cIter = vec.begin();
-                                          // cIter는 const T*처럼 동작
-  *cIter = 10;                            // 에러! *cIter가 상수이기 때문에 안됨
-  ++cIter;                                // 이건 문제없습니다. cIter를 변경
-}
+int main() {}
