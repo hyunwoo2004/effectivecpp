7408de5ab8ba7860d833a5d848cc133c343e7366
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-09 20:29:11 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,39 @@
 #include<iostream>
-#include <vector>
+#include <list>
+
 // Effective C++
 // 1. C++에 왔으면 C++ 법을 따릅시다.
 // 항목 4: 객체를 사용하기 전에 반드시 그 객체를 초기화하자
 
-// 가장 좋은 방법: 모든 객체를 사용하기 전에 항상 초기화(initialization)하는 것
-// 기본제공 타입으로 만들어진 비멤버 객체에 대해서는 초기화를 손수해야 함
-// 예시
-int main() {
-  int x = 0;                                  // int의 직접 초기화
-  const char* text = "A C-style string";      // 포인터의 직접 초기화
-                                              // (항목 3도 참조)
-
-  double d;                                   // 입력 스트림에서 읽음으로써
-  std::cin >> d;                              // "초기화"수행
-} 
-// 이런 부분을 제외하면, C++ 초기화의 나머지 부분은 생성자로 귀결됨
 // 생성자에서 지킬 규칙 - 그 객체의 모든 것을 초기화하자!
+// 쉬운 규칙 같지만 주의 할 점 -> 대입(Assignment)을 초기화(initialization)와 헷갈리지 않아야 함
+// 예시
+class PhoneNumber {...};
+
+class ABEtry {       // ABEtry = "Address Book Entry"
+public:
+  ABEtry(const std::string& name, const std::string& address,
+          const std::list<PhoneNumber>& phones);
+private:
+  std::string theName;
+  std::string theAddress;
+  std::list<PhoneNumber> thePhones;
+  int numTimesConsulted;
+};
+
+ABEtry::ABEtry(const std::string& name, const std::string& address,
+          const std::list<PhoneNumber>& phones) 
+{
+  theName = name;                    // 지금은 모두 '대입'을 하고 있음
+  theAddress = address;              // '초기화'가 아님
+  thePhones = phones;
+  numTimesConsulted = 0;
+}
+// 원하는 결과는 나오겠지만 썩 좋지 않음
+// C++ 규칙에 의하면 어떤 객체이든 그 객체의 데이터 멤버는 생성자의 본문이 실행되기 '전에' 초기화되어야 한다고 명시됨
+// 위에 있는 생성자에서 '대입'하기 전에 데이터 멤버는 기본 생성자가 호출됨 (기본제공 타입의 데이터 멤버는 제외)
+
+int main() {
+
+} 
+
