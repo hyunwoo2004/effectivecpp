413216cc1a0cc432330f6b0b7e64c80182633480
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-09 20:37:25 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,8 @@
 // 1. C++에 왔으면 C++ 법을 따릅시다.
 // 항목 4: 객체를 사용하기 전에 반드시 그 객체를 초기화하자
 
-// 생성자에서 지킬 규칙 - 그 객체의 모든 것을 초기화하자!
-// 해결법 - 초기화 리스트 사용
-// 예시
-ABEtry::ABEtry(const std::string& name, const std::string& address,
-          const std::list<PhoneNumber>& phones) 
-: theName {name},                    // 이제 이들은 모두 '초기화'되고 있음
-  theAddress {address},
-  thePhones {phones},
-  numTimesConsulted {0}
-{}                                   // 생성자 본문에 이제 아무것도 없음
-// 앞에 결과와 같지만, 지금의 생성자가 더 효율적일 가능성이 큼
-// 이전 방법: 각 데이터 멤버의 기본 생성자를 호출하고 복사 대입 연산자를 호출함
-// 위 방법: 각 데이터 멤버의 복사생성자만 호출
+// 기본제공 타입의 객체의 경우 두 방법 효율은 비슷함
+// 그러나 초기화 리스트를 하는 것을 추천
 
 int main() {
 
