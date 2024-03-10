f707649e292f5d0ff092ff8edba95788fd1bee62
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-10 19:31:24 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@
 // 항목 5: C++가 은근슬쩍 만들어 호출해버리는 함수들에 촉각을 세우자
 
 // 클래스가 비어 있지만(empty) 비어 있는게 아닌 때가 있음
-// 먼저 C++(컴파일러)가 빈 클래스를 훑고 지나갈 때를 생각해보자
-// -> C++의 어떤 멤버 함수는 우리가 클래스 안에 직접 선언해 넣지 않으면 컴파일러가 저절로 선언해 주도록 되어 있음
-// 1. 기본 생성자(Default Ctor)
-// 2. 복사 생성자(Copy Ctor)
-// 3. 복새 대입 연산자 (Copy assignment operator)
-// 4. 소멸자(Dtor)
-// 이들은 모두 기본형이고 public 멤버로 되어 있음
-// 심지어 inline 함수임
+// 에시
+class Empty {}; // 우리가 이렇게 썼다고 가정해 보자
 
+// --- 실제 컴파일러 ---
+class Empty {
+public:
+  Empty() {}                             // ctor
+  Empty(const Empty& rhs) {}             // copy ctor
+  ~Empty() {}                            // dtor: 가상 함수 여부에 대해서느
+                                         // 밑에서 더 자세히 설명하겠음
+  Empty& operator=(const Empty& rhs) {}  // copy assignment operator
+};
 
 int main() {
 
