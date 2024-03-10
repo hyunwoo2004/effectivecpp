f7dd171b3aa05716f77d4a5456bd5076f1a7a918
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-10 20:15:46 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,29 +6,40 @@
 
 // 그렇다면 컴파일러가 만드는 함수가 하는 일이 무엇이기에 저절로 만들어주는 것일까?
 // 2. 복사 생성자와 복사 대입 연산자
-// 컴파일러가 몰래 만들어낸 복사 생성자/대입 연산자가 하는 일은 매우 단순함
-// -> 원본 객체의 비정적 데이터를 사본 객체 쪽으로 그냥 복사만 함
-// 예시
-template<typename T>
-class NamedObject { 
-public:
-  NamedObject(const char* name, const T& value);
-  NamedObject(consst std::string& name, const T& value);
-private:
-  std::String nameValue;
-  T ObjectValue;
-};
-// 반면, 복사 생성자나 복사 대입 연산자는 NamedObject에 선언되어 있지 않기 때문에
-// 두 함수의 기본형이 컴파일러에 의해 만들어짐
-NamedObject<int> no1("Smallest Prime Number", 2);
-NameObject<int> no2(no1);  // 여기서 복사생성자가 호출됨
-
 // 컴파일러가 만들어주는 NameObject<int>의 복사 대입 연산자도 근본적으로 동작 원리는 같음
 // 일반적인 것만 보면, 복사 대입 연산자의 동작이 아까와 같게 되려면
 // -> 최종 결과 코드가
 // 1. 적법해야 하고(legal)
 // 2. 이치에 닿아야만(resonable) 함
 // 2개 중 하나라도 검사를 통과하지 못하면 컴파일러는 operator=의 자동생성을 거부함
+// 예시
+template<typename T>
+class NamedObject { 
+public:
+  // 이 생성자는 이제 상수 타입의 name을 취하지 않음. nameValue가
+  // 비상수 string의 참조자가 되었기 때문임. 참조할 string을 가져야 하기 
+  // 때문에 char*는 없애버림
+  NamedObject(consst std::string& name, const T& value);
+  //...
+                    // 위의 경우와 마찬가지로 operator=는
+                    // 선언된 게 없다고 가정
+private:
+  std::string& nameValue;    // 이제 이 멤버는 참조자임
+  const T ObjectValue;       // 이제 이 멤버는 상수임.
+};
+// 여기서 어떤 일이 일어날 지는 밑에서 보자
+std::string newDog("Persephone");
+std::string oldDog("Satch");
+
+NameObject<int> p(newDog, 2); 
+NameObject<int> s(oldDog, 36); 
+
+p = s;                         // p에 들어있는 데이터 멤버에서 어떤 일이
+                               // 일어날까요?
+// 대입 연산이 일어나기 전, p.nameValue와 s.nameValue는 각각 다른 string 객체를 참조하고 있음
+// p의 string참조자 자체를 바꿔야 하나? -> C++의 참조자는 원래 자신이 참조하고 있는 것과 다른 객체를 참조할 수 없음
+// p가 참조하는 newDog를 바꿔야 하나? -> 그 string에 대한 포인터나 참조자를 품고 있는 다른 객체들, 즉 실제 대입 연산에
+// 직접적으로 관여하지 않는 객체까지 영향을 받게 됨 -> 마땅한 일이 아님 (연산자 자동 생성 거부)
 
 int main() {
 
