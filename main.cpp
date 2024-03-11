f6a4ca304138a1d1c1aab500943c2d754c4a1a69
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-11 17:33:06 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,8 +20,17 @@ class WristClock : public TimeKeeper {...};
 // -> 어떤 시간 기록 객체에 대한 포인터를 손에 넣는 용도로 팩토리 함수(factory function, 새로 생성된 파생 클래스 객체에 대한 기본 클래스 포인터를 반환하는 함수)
 //    를 만들기
 TimeKeeper* getTimeKeeper();
-
+// 팩토리 함수의 기존 규약을 그대로 따라간다면 getTimeKeeper 함수에서 반환되는 객체는 힙(Free store의 옛 명칭, 영어로는 Heap)에 있게 되므로
+// 결국 메모리 및 기타 자원의 누출을 막기 위해 해당 객체를 적절히 delete 해야 함
 int main() {
-
+  TimeKeeper* ptk = getTimeKeeper();    // TimeKeeper클래스 계통으로부터
+                                        // 동적으로 할당된 객체를 얻음
+  
+  ...                                   // 이 객체를 사용
+  
+  delete ptk;                           // 자원 누출을 막기 위해 해제(삭제)함
 } 
+// 아쉬운 점 - 우선 지나가는 이야기
+// 객체 삭제를 사용자에게 전가하는 것은 에러 발생에 노출될 소지가 있음
+// 팩토리 함수의 인터페이스를 수정하면 흔히 발생할 수 있는 사용자 에러를 방지할 수 있음 
 
