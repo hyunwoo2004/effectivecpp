707344fb0cd15fe3177f8c1e4e1403fb8bdc4e68
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-12 18:34:14 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,39 +9,42 @@
 class DBConnection {
 public:
   ...
-  static BDConnection create(); // DBConnection 객체를 반환하는
-                                // 함수. 매개변수는 편의상 생략함
+  static BDConnection create();
+                             
 
-  void close();                 // 연결을 닫음. 이때,
-};                              // 연결을 실패하면 예외를 던짐
-// 사용자가 BDConnection 객체에 대해 close를 직접 호출해야 함
-// 사용자의 망각을 사전에 차단하기 위해 DBConnection에 대한 자원 관리 클래스를 만듦(소멸자에 close() 함수를 호출)
-class DBConn {                  // DBConnection 객체를 관리하는 
-public:                         // 클래스
+  void close();        
+};  
+
+class DBConn {              
+public:                       
   ...
-  ~DBConn() {                   // 데이터베이스 연결이 항상 닫히도록
-    db.close();                 // 확실히 챙겨주는 함수
-  }
+  ~DBConn();
 private:
   DBConnection db;
 };
-// 이를 통해 다음과 같은 프로그래밍이 가능
-int main() 
-{                                        // 블록 시작
-  DBConn dbc(DBConnection::create());    // DBConnection 객체를 생성하고
-                                         // 이것을 DBConn 객체로 넘겨서
-                                         // 관리를 맡김
-
-                                         // DBConn 인터페이스를 통해
-                                         // 그 DBConnection 객체를 사용
-  
-}                                        // 블록 끝. DBConn 객체가
-                                         // 여기서 소멸됨. 따라서
-                                         // DBConnection 객체에 대한 close
-                                         // 함수의 호출이 자동으로 이루어짐 
-
 // close 호출만 일사천리로 성공하면 아무런 문제가 없는 코드
 // 그러나 close를 호출했는데 예외가 발생한다면?
 // -> DBConn의 소멸자는 분명히 이 예외를 전파할 것
 //    쉽게 말해, 그 소멸자에서 예외가 나가도록 내버려 두게 됨 (문제 발생!!!)
 // 예외를 던지는 소멸자 --> 우리의 '걱정거리'
+
+// 이 문제를 피하는 방법
+// 1. close에서 예외가 발생하면 프로그램을 바로 끝냄 - 대부분 abort()를 호출함
+DBConn::~DBConn() 
+{
+  try { db.close(); }
+  catch (...) {
+    // close 호출이 실패했다는 로그를 작성
+    std::abort();
+  }
+}
+// 객체 소멸이 진행되다가 에러가 발생한 후에 프로그램 실행을 계속할 수 없는 상황이라면 꽤 괜찮은 선택임
+//   소멸자에서 생긴 예외를 그대로 흘려 내보내다가 정의되지 않은 동작에까지 이를 수 있다면
+// 이런 불상사를 막는다는 의미에서 어느 정도 장점도 있음
+// 간단히 말하면, abort를 호출해서 못 볼꼴을 미리 안 보여 주겠다는 의도
+
+int main() 
+{
+  
+}
+
