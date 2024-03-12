b357c8eb2f9812856ae8286f0a0a76aedc67ba53
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-12 18:19:19 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,6 +15,18 @@ public:
   void close();                 // 연결을 닫음. 이때,
 };                              // 연결을 실패하면 예외를 던짐
 // 사용자가 BDConnection 객체에 대해 close를 직접 호출해야 함
+// 사용자의 망각을 사전에 차단하기 위해 DBConnection에 대한 자원 관리 클래스를 만듦(소멸자에 close() 함수를 호출)
+class DBConn {                  // DBConnection 객체를 관리하는 
+public:                         // 클래스
+  ...
+  ~DBConn() {                   // 데이터베이스 연결이 항상 닫히도록
+    db.close();                 // 확실히 챙겨주는 함수
+  }
+private:
+  DBConnection db;
+};
+
+
 
 int main() {
 
