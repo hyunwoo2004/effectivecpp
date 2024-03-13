cc36671886482a56d95f654e058463b8a41bea1f
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-13 17:06:39 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,9 +34,15 @@ public:
                                                  // 로깅을 구현함
  ...
 };
-
+// 이제 아래의 코드가 실행될 때 어떻게 되는지 생각해보자.
 int main() 
 {
-  
+ BuyTransaction b;
 }
+// BuyTransaction 생성자가 호출되는 것은 맞음. 그러나 우선은 Transaction 생성자가 호출되어야 함
+// 파생 클래스 객체가 생성될 때 그 객체의 기본 클래스 부분이 파생 클래스 부분보다 먼저 호출되는 것이 정석
+// Transaction 생성자의 마지막 줄을 보면 가상 함수인 logTransaction을 호출하는 문장이 보이는데 무척 당혹스러운 사건이 벌어지는 부분이 바로 이 문장임
+// 여기서 logTransaction 함수는 BuyTransaction의 것이 아니라 Transaction의 것이란 사실! (지금 생성되는 객체의 타입이 BuyTransaction 인데도)
+
+
 
