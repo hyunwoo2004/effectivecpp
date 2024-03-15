17444789254f9ca59490b962e0f3f349fad5272c
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-15 20:56:59 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,7 @@
 // 2. 생성자, 소멸자 및 대입 연산자
 // 항목 12: 객체의 모든 부분을 빠짐없이 복사하자
 
-Customer::Customer(const Customer& rhs) 
-  : name(rhs.name)                                 // rhs의 데이터를 복사함
-{
-  logCall("Customer copy constructor");
-}
-
-Customer& operator=(const Customer& rhs) 
-{
-  logCall("Customer copy assignmenr operator");
-  
-  name = rhs.name;                                 // rhs의 데이터를 복사함
-   
-  return *this;                                    // 항목 10 참고
-}
-// 문제될 것이 하나도 없어 보임. 실제로 그렇다
 // 그런데 데이터 멤버 하나를 Customer에 추가하면서 행복에 금이 가기 시작함
-
 class Date {...};                                  // 날짜 정보를 위한 클래스
 
 class Customer {
@@ -28,7 +12,10 @@ public:
   ...                                              // 이전과 동일
 private:
   std::string name;
+  Date lastTransaction;
 };
+// 이렇게 되고 나면 복사 함수의 동작은 완전 복사가 아니라 부분 복사(partial copy)가 됨
+// 고객의 name은 복사하지만, lastTransaction은 복사하지 않음
 
 int main() 
 {
