f87a0a812ab1c3864cf0172141d27eb6a22c0ab8
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-15 21:15:11 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,6 +36,9 @@ PriorityCustomer::operator=(const PriorityCustomer& rhs)
 // PriorityCustomer 클래스의 복사 함수는 언뜻 보기엔 PriorityCustomer의 모든 것을 복사하고 있는 것처럼 보이지만, 다시 한번 보라!
 //  PriorityCustomer에 선언된 데이터 멤버를 모두 복사하고 있는 것은 사실이지만, Customer로부터 상속한 데이터 멤버들의 사본도 엄연히 
 // PriorityCustomer클래스에 들어 있는데, 이들은 복사가 안 되고 있음!!!
+//  PriorityCustomer의 복사 생성자에는 기본 클래스 생성자에 넘길 인자들도 명시되어 있지 않아서, PriorityCustomer 객체의 Customer 부분은 
+// 인자 없이 실행되는 Customer 생성자, 즉 기본 생성자에 의해 초기화됨
+// 이 생성자는 당연히 name 및 last-Transaction에 대해 '기본적인' 초기화를 해 줌
 
 int main() 
 {
