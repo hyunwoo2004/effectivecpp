d3de43d57b82077a9848a6ea03a086ed72d024c0
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-16 18:15:40 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@
 
 // 사정이 어쨌든, 파생 클래스에 대한 복사 함수를 우리가 스스로 만든다고 결심했으면
 // 기본 클래스 부분을 복사해서 빠뜨리지 않도록 각별히 주의하자!
+// 물론 기본 클래스 부분은 private 멤버일 가능성이 높기 때문에(항목 22 참조), 이들을 직접 건드리긴 어려움
+// 그 대신, 파생 클래스의 복사 함수 안에서 기본 클래스의 복사 함수를 호출하도록 만들면 됨
+PriorityCustomer::PriortyCustomer(const PriorityCustomer& rhs)
+  : Customer(rhs),                                               // 기본 클래스의 복사 생성자를 호출함
+    Priority(rhs.Priority)
+{
+  logCall("PriorityCustomer copy constructor");
+}
 
 int main() 
 {
