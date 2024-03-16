898a4f81cdf8d96c6b9004bb90a46a5c860fda2f
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-16 18:02:22 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 // 결국 우리가 할 일은 한 가지 - 클래스에 데이터 멤버를 추가했으면, 추가한 데이터 멤버를 처리하도록 복사 함수를 다시 작성해야 함
 // 비표준형 operator= 함수도 전부 바꿔 줘야 함
 
-// 이 문제가 가장 사악하게 프로그래머를 괴롭히는 경우가 하나 있는데, 바로 클래스 생속임
+// 이 문제가 가장 사악하게 프로그래머를 괴롭히는 경우가 하나 있는데, 바로 클래스 상속임
 // 예시
 class PriorityCustomer : public customer {
 public:
@@ -39,6 +39,9 @@ PriorityCustomer::operator=(const PriorityCustomer& rhs)
 //  PriorityCustomer의 복사 생성자에는 기본 클래스 생성자에 넘길 인자들도 명시되어 있지 않아서, PriorityCustomer 객체의 Customer 부분은 
 // 인자 없이 실행되는 Customer 생성자, 즉 기본 생성자에 의해 초기화됨
 // 이 생성자는 당연히 name 및 last-Transaction에 대해 '기본적인' 초기화를 해 줌
+
+// PriorityCustomer의 복사 대입 연산자의 경우에는 사정이 다소 다름
+// 복사 대입 연산자는 기본 클래스의 데이터 멤버를 건드릴 시도도 하지 않기 때문에, 기본 클래스의 데이터 멤버는 변경되지 않고 그대로 있게 됨
 
 int main() 
 {
