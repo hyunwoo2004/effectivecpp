86f213538b4f73b24b622830ecd1eec8be1cdc7c
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-17 20:47:00 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,18 @@
 // 3. 자원 관리
 // 항목 13: 자원 관리에는 객체가 그만!
 
-// auto_ptr를 쓰 수 없는 상황이라면 그 대안으로 '참조 카운팅 방식 스마트 포인터'(reference-counting smart pointer: RCSP)가 아주 괜찮음
-// RCSP는 특정한 어떤 자원을 가리키는 외부 객체의 개수를 유지하고 있다가 그 개수가 0이 되면 해당 자원을 자동으로 삭제하는 스마트 포인터임
-// 이것만 보면, RCSP의 동작은 가비지 컬렉션(garbage collection)의 그것과 상당히 흡사
-// 단, 참조 상태가 고리를 이루는 경우를 없앨 수 없다는 점은 기비지 컬렉션과 다름
+// TR1에서 제공되는 tr1::shared_ptr(항목 54참조)이 대표적인 RCSP임
+// 이것을 써서 f 함수를 다시 작성하면 이렇다
+void f() 
+{
+  ...
+  std::tr1::shared_ptr<Investment>
+    pInv1(createInvestment());             // 팩토리 함수 호출
+
+  ...                                      // 예전처럼 pInv를 사용
+
+}                                          // shared_ptr의 소멸자를 통해
+                                           // pInv를 자동으로 삭제
 
 int main() 
 {
