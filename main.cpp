033a15bba27cb57215ab41ed20204b4671c5831c
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-17 20:13:46 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,18 @@
 // -> 만에 하나 이런한 사태가 되면 결국 자원이 두 번 삭제되는 결과를 낳게 되고, 프로그램은 미정의 동작의 수렁에 빠지게 욈
 // 이를 막기 위해 auto_ptr에는 유별난 특성이 있음 - auto_ptr 객체를 복사하면, 원본 객체는 null로 만듦
 //                                                  (복사하는(copying) 객체만이 그 자원의 유일한 소유권(ownership)을 갖는다고 가정)
-
+// 예시
 int main() 
 {
+  std::auto_ptr<Investment>                                 // pInv1이 가리키는 대상은
+              pInv1(createInvestment());                    // createInvestment 함수에서
+                                                            // 반환된 객체임
 
+  std::auto_ptr<Investment> pInv2(pInv1);                   // pInv2는 현재 그 객체를 가리키고
+                                                            // 있는 반면, 이제 pInv1은 null임
+  
+  pInv1 = pInv2;                                            // 지금 pInv1는 그 객체를 가리키고
+                                                            // 있으며, pInv2은 null임
 }
 
 
