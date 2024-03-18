12c8dae9a96fdbe34697e81d04b8494279529e83
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-18 17:44:22 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,11 @@
 // 3. 자원 관리
 // 항목 13: 자원 관리에는 객체가 그만!
 
-// TR1에서 제공되는 tr1::shared_ptr(항목 54참조)이 대표적인 RCSP임
-// 이것을 써서 f 함수를 다시 작성하면 이렇다
-void f() 
-{
-  ...
-  std::tr1::shared_ptr<Investment>
-    pInv1(createInvestment());             // 팩토리 함수 호출
-
-  ...                                      // 예전처럼 pInv를 사용
-
-}                                          // shared_ptr의 소멸자를 통해
-                                           // pInv를 자동으로 삭제
-
 // auto_ptr을 사용한 버전과 거의 똑같아 보이는 코드이지만, shared_ptr의 복사가 훨씬 자연스러워졌음
 // 예시
 void f() 
 {
-  ...
+  //...
   std::tr1::shared_ptr<Investment>         // pInv1이 가리키는 대상은
     pInv1(createInvestment());             // createInvestment 함수에서
                                            // 반환된 객체임
@@ -32,9 +19,11 @@ void f()
   pInv1 = pInv2;                           // 마찬가지 - 변한 것도 하나도
                                            // 없음
 
-  ...                                      // pInv1 및 pInv2는 소멸되며,
+  //...                                    // pInv1 및 pInv2는 소멸되며,
 }                                          // 이들이 가르키고 있는 객체도
                                            // 자동으로 삭제됨
+// 복사 동작이 '예상대로' 이루어지기 때문에, tr1::shared_ptr은 괴상한 복사 동작으로
+// 인해 auto_ptr을 쓸 수 없는 STL 컨테이너 등의 환경에 딱 맞게 쓸 수 있음
 
 int main() 
 {
