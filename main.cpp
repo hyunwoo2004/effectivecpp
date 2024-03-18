eefa37015f94631f6731f0aba0cb978bfbcbb3a5
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-18 17:40:11 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,6 +17,25 @@ void f()
 }                                          // shared_ptr의 소멸자를 통해
                                            // pInv를 자동으로 삭제
 
+// auto_ptr을 사용한 버전과 거의 똑같아 보이는 코드이지만, shared_ptr의 복사가 훨씬 자연스러워졌음
+// 예시
+void f() 
+{
+  ...
+  std::tr1::shared_ptr<Investment>         // pInv1이 가리키는 대상은
+    pInv1(createInvestment());             // createInvestment 함수에서
+                                           // 반환된 객체임
+
+  std::tr1::shared_ptr<Investment>         // pInv1 및 pInv2가 동시에
+    pInv2(pInv1);                          // 그 객체를 가리키고 있음
+                                           
+  pInv1 = pInv2;                           // 마찬가지 - 변한 것도 하나도
+                                           // 없음
+
+  ...                                      // pInv1 및 pInv2는 소멸되며,
+}                                          // 이들이 가르키고 있는 객체도
+                                           // 자동으로 삭제됨
+
 int main() 
 {
 
