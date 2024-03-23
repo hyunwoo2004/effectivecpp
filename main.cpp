a93c83752ca0dad945b961d0a94cf5069e7cc268
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-23 13:15:26 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 
 // Effective C++
 // 3. 자원 관리
-// 항목 16: new 및 delete를 사용할 떄는 형태를 반드시 맞추자
+// 항목 17: new로 생성한 객체를 스마트 포인터에 저장하는 코드는 별도의 한 문장으로 만들자
 
-// 이것만은 잊지말자!
-// - new 표현식에 []를 썼으면, 대응되는 delete 표현식에도 []를 써야 함. 마찬가지로
-//   new 표현식에 []를 안 썼으면, 대응되는 delete 표현식에도 []를 쓰지 말아야 함
+// 처리 우선순위를 알려 주는 함수가 하나 있고, 동적으로 할당한 Widget 객체에 대해 어떤 우선순위에 따라 처리를 적용하는 함수가 하나 있다고 가정함
+
+int priority{};
+
+void processWidget(std::tr1::shared_ptr<Widget> pw, int priority);
 
 int main() 
 {
