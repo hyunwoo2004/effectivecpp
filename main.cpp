c7d28c0f0ef57cf8cf909f9f7baca972b9f058aa
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-26 20:42:01 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 
 // 이런 가정도 한번 해 보자 -  createInvestment를 통해 얻은 Investment* 포인터를 직접 삭제하지 않게 하고 
 //                             getRidOfInvestment라는 이름의 함수를 준비해서 여기에 넘기게 하면 어떤가?
+// 왠지 더 깔끔해 보이지만 이런 인터페이스는 되레 사용자 실수를 하나 더 열어놓는 결과를 가져옴
+// 자원 해제 매커니즘을 잘못 사용할 수가 있음 (getRidOfInvestment를 잊고 delete를 쓰는 경우)
 
 int main() 
 {
