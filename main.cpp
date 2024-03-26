b9f8f745e794abd170e0a64d0659de1dc51d7d96
hyunwoo2004 <148432321+hyunwoo2004@users.noreply.github.com>
2024-03-26 21:20:19 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 // 이 또 하나의 잘못이란 바로 '교차 DLL 문제(cross-DLL problem)'임
 // 이 문제가 생기는 경우가 언제냐 하면, 객체 생성 시에 어떤 동적 링크 라이브러리(dynamically linked library: DLL)의 new를 썼는데
 // 그 객체를 삭제할 떄는 이전의 DLL과 다른 DLL에 있는 delete을 썼을 경우임
+// 이렇게 new/delete 짝이 실행되는 DLL이 달라서 꼬이게 되면 대다수의 플랫폼에서 런타임 에러가 일어남
+// 그런데 tr1::shared_ptr은 이 문제를 피할 수 있음
 
 int main() 
 {
